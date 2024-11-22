bd4478f365656ba37f9565fe5fd3556f17ebf7d9
Abd Alrazzaq <devoojs@gmail.com>
2024-11-22 16:02:45 +0300

    update src/js/navbar.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -6,13 +6,59 @@ class Header extends HTMLElement {
   connectedCallback() {
 
     this.innerHTML = `
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add an active class to highlight the current page */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Hide the link that should open and close the topnav on small screens */
+.topnav .icon {
+  display: none;
+}
+
+</style>
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
   <style>
     nav {
       height: 40px;
       display: flex;
       align-items: center;
       justify-content: center;
-      background-color: #007bff;
+      background-color: #FAB12F;
     }
 
     ul {
@@ -38,6 +84,7 @@ class Header extends HTMLElement {
   </style>
   <header>
     <nav>
+    <p>devoojs</p>
       <ul>
         <li><a href="web.html">Home</a></li>
         <li><a href="work.html">Work</a></li>
@@ -46,6 +93,16 @@ class Header extends HTMLElement {
     </nav>
   </header>
 `;
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
   }
 }
 customElements.define('header-component',Header);
