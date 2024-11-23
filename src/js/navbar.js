ef8ad88a31eae25050b01cba59c972e9048544af
Abd Alrazzaq <devoojs@gmail.com>
2024-11-23 21:58:29 +0300

    update src/js/navbar.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -10,7 +10,7 @@ customElements.define('header-component',class Header extends HTMLElement {
       display: flex;
       align-items: center;
       justify-content: center;
-      background-color: #7f7f7f
+      background-color: #f1f1f1
     }
 
     ul {
