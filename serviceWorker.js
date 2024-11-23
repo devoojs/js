684469d367fdf1b3cd6c906576cec287ea7c8957
Abd Alrazzaq <devoojs@gmail.com>
2024-11-23 17:23:28 +0300

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,5 @@
-const staticDevCoffee = "dev-coffee-site-v1";
-const assets = [
-  "/",
-  "/index.html",
-  "/src/js.navbar",
-  "/header.js",
-  "/paw.js",
-];
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+  // This is a dummy event listener
+  // just to pass the PWA installation criteria on 
+  // some browsers
 });
